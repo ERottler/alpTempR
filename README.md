7518cddf193e0af92270e769f73e8ba4ea0ecd5f
Erwin Rottler <rottler@uni-potsdam.de>
2018-04-27 10:41:02 +0200

    Update README. Link for web app.

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 +++UNDER CONSTRUCTION+++
 
-R package for analysis of alpine climatological data using moving average trend statistics.
+Elevation-dependent warming in the Swiss Alps 1981-2017 : features, forcings and feedbacks
+
+R package for the analysis of alpine climatological data using moving average trend statistics.
+
+This repository stores the code used for analyzing alpine climate data from Switzerland to address the following reseach questions: 
+- Are temperatures in Switzerland changing at a different rate depending on elevation?
+- What are underlying driving mechanisms of elevation dependent temperature changes?
+
+Within this context, we place special attention on mechanisms related to changes in incoming solar radiation, cloud cover, air humidity, snow cover and large-scale circulation.
+
+First results can be viewed at: http://seca-vista.geo.uni-potsdam.de:3838/AlpTempApp/
--- data-raw/data_vis.R
+++ data-raw/data_vis.R
@@ -10,9 +10,11 @@ stat_meta <- read.table(paste0(base_dir,"rawData/IDAweb/stationMeta.csv"), sep="
 
 #cycl_elev_mea----
 
-pdf(paste0("u:/RhineFlow/Elevation/cycl_elev_mea.pdf"), width = 6.7, height = 8)
+# pdf(paste0("u:/RhineFlow/Elevation/cycl_elev_mea.pdf"), width = 6.7, height = 8)
 # png(paste0("u:/RhineFlow/Elevation/cycl_elev_mea.png"), width = 6.7, height = 8,
 #     units = "in", res = 100)
+tiff(paste0("u:/RhineFlow/Elevation/cycl_elev_mea.tiff"), width = 6.7, height = 8,
+    units = "in", res = 800)
 
 par(oma=c(0,0,0,0))
 par(family="serif")
@@ -68,9 +70,11 @@ dev.off()
 
 #cycl_elev_slo----
 
-pdf(paste0("u:/RhineFlow/Elevation/cycl_elev_slo.pdf"), width = 6.7, height = 8)
+# pdf(paste0("u:/RhineFlow/Elevation/cycl_elev_slo.pdf"), width = 6.7, height = 8)
 # png(paste0("u:/RhineFlow/Elevation/cycl_elev_slo.png"), width = 6.7, height = 8,
 #     units = "in", res = 100)
+tiff(paste0("u:/RhineFlow/Elevation/cycl_elev_slo.tiff"), width = 6.7, height = 8,
+     units = "in", res = 800)
 
 par(oma=c(0,0,0,0))
 par(family="serif")
@@ -139,9 +143,11 @@ pos_iso_text[2, ] <- c(67, 52, 34, 12)
 my_col <- colorRampPalette(brewer.pal(11,"RdYlBu"))(100); my_col <- my_col[length(my_col):1]
 
 
-pdf("u:/RhineFlow/Elevation/imag_elev_slo.pdf", width = 6.7, height = 7)
+# pdf("u:/RhineFlow/Elevation/imag_elev_slo.pdf", width = 6.7, height = 7)
 # png(paste0("u:/RhineFlow/Elevation/imag_elev_slo.png"), width = 6.7, height = 7,
 #     units = "in", res = 1200)
+tiff(paste0("u:/RhineFlow/Elevation/imag_elev_slo.tiff"), width = 6.7, height = 8,
+     units = "in", res = 800)
 
 par(oma=c(0,0,0,0))
 par(family="serif")
